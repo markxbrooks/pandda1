450e6641356083b7322a4ff3c9d998167cd0d273
Brooks, Mark <mark.brooks@evotec.com>
2025-12-09 18:01:34 -0500

    panddaanalyse.bat

--- bin/pandda.analyse.bat
+++ bin/pandda.analyse.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Adjust paths as needed
 
-SET VENV=C:\Users\MBrooks\projects\pandda-bitbucket\venv
-SET PYTHONPATH=C:\Users\MBrooks\projects\pandda-bitbucket\lib-python;%PYTHONPATH%
+SET CBIN=C:\Users\MBrooks\CCP4-9\CCP4-9\CCP4\bin
+SET PYTHONPATH=C:\Users\MBrooks\projects\pandda1\lib-python;%PYTHONPATH%
 
-"ccp4-python" -m pandda.analyse %*
+"%CBIN%\ccp4-python.bat" -m pandda.analyse %*
